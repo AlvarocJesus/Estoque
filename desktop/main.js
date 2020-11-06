2d82d6cd9b245172d2dc2fe2b1a52293b950c110
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2020-11-05 23:19:10 -0300

    Mudando organizacao das pastas, inicio da estilizicacao da pagina principal

--- desktop/main.js
+++ desktop/main.js
@@ -3,14 +3,14 @@ const { app, BrowserWindow } = require('electron');
 function createWindow(){
     const win = new BrowserWindow({
         width: 800,
-        altura: 600,
+        height: 600,
         webPreferences: {
             nodeIntegration: true,
         }
     })
 
-    win.loadFile('index.html');
-    
+    win.loadFile('./src/views/index.html');
+    win.webContents.openDevTools()
 }
 
-app.whenReady().then();
+app.whenReady().then(createWindow);
