cd0c41f5b0cf7498135d206577c8da0f96868301
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2020-12-20 22:35:11 -0300

    Tentando resolver bug

--- back/src/database/migrations/20201027192347_create_product.js
+++ back/src/database/migrations/20201027192347_create_product.js
@@ -2,11 +2,11 @@
 exports.up = knex => knex.schema.createTable('products', table => {
     table.increments('id').primary()
     table.string('name').unique().notNullable();
-    table.decimal('preco').notNullable();
+    table.integer('preco').notNullable();
     table.integer('quantidade').notNullable();
-    table.decimal('precoUnitario').notNullable();
+    table.integer('precoUnitario').notNullable();
     table.string('fornecedor');
-    table.decimal('precoCusto');
+    table.integer('precoCusto');
 })
 
 exports.down = knex => knex.schema.dropTable('products');
--- desktop/src/scripts/addProduct.js
+++ desktop/src/scripts/addProduct.js
@@ -1,46 +1,25 @@
 import api from '../service/api.js';
 
 const name = document.getElementById('name');
-
 const preco = document.getElementById('preco');
-    
 const quantidade = document.getElementById('quantidade');
-
 const precoUnitario = document.getElementById('valor-unitario');
-
 const fornecedor = document.getElementById('fornecedor');
-
 const precoCusto = document.getElementById('preco-custo');
 
+const btn = document.getElementById('btn');
+btn.addEventListener('click', SubmitData);
+
 function SubmitData(){
-    // alert(`produto: ${name.value} \n preco: ${preco.value} \n quantidade: ${quantidade.value} \n precoUnitario: ${precoUnitario.value} \n fornecedor: ${fornecedor.value} \n precoCusto: ${precoCusto.value} \n`);
 
     api.post('/add', {
-        name: name,
-        preco: preco,
-        quantidade: quantidade,
-        precoUnitario: precoUnitario,
-        fornecedor: fornecedor,
-        precoCusto: precoCusto,
+        name: name.value,
+        preco: preco.value,
+        quantidade: quantidade.value,
+        precoUnitario: precoUnitario.value,
+        fornecedor: fornecedor.value,
+        precoCusto: precoCusto.value,
     })
         .then(res => console.log(res))
         .catch(err => console.log(err))
 }
-
-// // console.log(name);
-// // console.log(preco);
-// // console.log(quantidade);
-// // console.log(precoUnitario);
-// // console.log(fornecedor);
-// // console.log(precoCusto);
-
-// api.post('/add', {
-//     name: name,
-//     preco: preco,
-//     quantidade: quantidade,
-//     precoUnitario: precoUnitario,
-//     fornecedor: fornecedor,
-//     precoCusto: precoCusto,
-// })
-//     .then(res => console.log(res))
-//     .catch(err => console.log(err))
