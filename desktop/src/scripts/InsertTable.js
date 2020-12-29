0a303b45119208acf5e0d52e1248630516222629
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2020-12-28 22:58:08 -0300

    Criacao e finalizacao da feature de adicionar e remover da lista

--- desktop/src/scripts/InsertTable.js
+++ desktop/src/scripts/InsertTable.js
@@ -1,78 +1,45 @@
 import api from '../service/api.js';
 
+const table = document.getElementById('table');
+
 api.get('list')
     .then((res) => {
         const product = res.data;
         
         product.map(item => {
-            const id = item.id;
-            const select = document.getElementById("product");
+            const linha = document.createElement("tr");
+            linha.addEventListener("click", function(){
 
-            const option = document.createElement("option");
-            option.setAttribute("value", id)
-            option.innerHTML = item.name;
-            
+                const linha2 = linha;
+                linha2.addEventListener("click", function(){
 
-            select.appendChild(option);
-
-            select.addEventListener('change', function(id){
-                // alert(this.value);
-
-                const linha = document.createElement("tr");
-
-                const celula = document.createElement("td");
-                celula.innerHTML = item.name;
-                
-
-                const celula1 = document.createElement("td");
-                celula1.innerHTML = item.preco;
-
-                const celula2 = document.createElement("td");
-                celula2.innerHTML = item.quantidade;
-
-                const celula4 = document.createElement("td");
-                celula4.innerHTML = item.fornecedor;
-
-                const celula5 = document.createElement("td");
-                celula5.innerHTML = item.precoCusto;
-
-
-                tabela.appendChild(linha);
-                    linha.appendChild(celula);
-                    linha.appendChild(celula1);
-                    linha.appendChild(celula2);
-                    linha.appendChild(celula4);
-                    linha.appendChild(celula5);
+                    tabela.appendChild(linha);
+                })
+                table.appendChild(linha2);
             })
-            // const value = select.options[select.selectedIndex].value;
+
+            const celula = document.createElement("td");
+            celula.innerHTML = item.name;
+
+            const celula1 = document.createElement("td");
+            celula1.innerHTML = item.preco;
+
+            const celula2 = document.createElement("td");
+            celula2.innerHTML = item.quantidade;
+
+            const celula4 = document.createElement("td");
+            celula4.innerHTML = item.fornecedor;
+
+            const celula5 = document.createElement("td");
+            celula5.innerHTML = item.precoCusto;
+
+            tabela.appendChild(linha);
+                linha.appendChild(celula);
+                linha.appendChild(celula1);
+                linha.appendChild(celula2);
+                linha.appendChild(celula4);
+                linha.appendChild(celula5);
+            })
             
-
-            // const linha = document.createElement("tr");
-
-            // const celula = document.createElement("td");
-            // celula.innerHTML = item.name;
-            
-
-            // const celula1 = document.createElement("td");
-            // celula1.innerHTML = item.preco;
-
-            // const celula2 = document.createElement("td");
-            // celula2.innerHTML = item.quantidade;
-
-            // const celula4 = document.createElement("td");
-            // celula4.innerHTML = item.fornecedor;
-
-            // const celula5 = document.createElement("td");
-            // celula5.innerHTML = item.precoCusto;
-
-
-            // tabela.appendChild(linha);
-            //     linha.appendChild(celula);
-            //     linha.appendChild(celula1);
-            //     linha.appendChild(celula2);
-            //     linha.appendChild(celula4);
-            //     linha.appendChild(celula5);
-        })
-
     })
     .catch((err) => console.log(`Erro ${err}`))
